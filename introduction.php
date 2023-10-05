60e50fd8c4cc73eff887c8754dc612ad880ba8d9
M303user <m303user@M303.uc-bcf.edu.ph>
2023-10-05 17:58:14 +0800

    introduction testing

---
+++ introduction.php
@@ -0,0 +1,14 @@
+<?php
+ 
+ $name = "November Nicomedez";
+ $age = 19;
+ $address = "Manaoag, Pangasinan";
+
+ $introduction = print($name);
+ $introduction = print($age);
+ $introduction = print($address);
+
+ $introduction = print("$name"+ "$age"+ $address);
+
+ print($introduction);
+?>
